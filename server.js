32c3cc3400020f167355770fd4b2835e2bd9b170
bobtv60 <105109017+bobtv60@users.noreply.github.com>
2024-11-04 17:19:35 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const User = require('./../models/user');
+const bcrypt = require('bcrypt');
 
 app.use(express.json());
 
@@ -25,6 +27,87 @@ app.post('/submit', (req, res) => {
     res.status(400).send('Please provide both name and message.');
   }
 });
+
+app.post('/signup', (req, res) => {
+    let {name, email, password, dateOfBirth} = req.body;
+    name = name.trim();
+    email = email.trim();
+    password = password.trim();
+    dateOfBirth = dateOfBirth.trim();
+
+    if (name == "" || email == "" || password == "" || dateOfBirth == "") {
+        res.json({
+            status: "FAILED",
+            message: "Empty input fields"
+        })
+    } else if (!/^[a-zA-Z ]+$/.test(name)) {
+        res.json({
+            status: "FAILED",
+            message: "Invalid name entered"
+        })
+    } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+        res.json({
+            status: "FAILED",
+            message: "Invalid email entered"
+        })
+    } else if (!new Date(dateOfBirth).getTime()) {
+        res.json({
+            status: "FAILED",
+            message: "Invalid date of birth entered"
+        })
+    } else if (password.length < 8) {
+        res.json({
+            status: "FAILED",
+            message: "Password is too short"
+        })
+    } else {
+        User.find({email}).then(result => {
+            if (result.length) {
+                res.json({
+                    status: "FAILED",
+                    message: "User with the provided email already exists"
+                })
+            } else {
+                const saltRounds = 10;
+                bcrypt.hash(password, saltRounds).then(hashedPassword => {
+                    const newUser = new User({
+                        name,
+                        email,
+                        password: hashedPassword,
+                        dateOfBirth
+                    })
+
+                    newUser.save().then(result => {
+                        res.json({
+                            status: "SUCCESS",
+                            message: "Signup successful",
+                            data: result,
+                        })
+                    })
+                    .catch(err => {
+                        res.json({
+                            status: "FAILED",
+                            message: "An error occured while saving user account"
+                        })
+                    })
+                })
+                .catch(err => {
+                    res.json({
+                        status: "FAILED",
+                        message: "An error occured while hashing password"
+                    })
+                })
+            }
+        }).catch(err => {
+            console.log(err);
+            res.json({
+                status: "FAILED",
+                message: "An error occured while checking for existing user"
+            })
+        })
+    }
+})
+
 
 // Start the server
 app.listen(PORT, () => {
